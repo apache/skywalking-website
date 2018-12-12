1e442bd3c2d90857a2314bd4522675553b42d250
TinyAllen <allen.wang.123@outlook.com>
2018-12-12 15:40:42 +0800

    Feature: new website

---
+++ docs/.vuepress/theme/components/Footer.vue
@@ -0,0 +1,100 @@
+<template>
+  <footer class="footer-container">
+    <div class="cols-container">
+      <div class="col col-18">
+          <dl>
+        <dt>Disclaimer</dt>
+        <dd>
+        <p>Apache SkyWalking (incubating) is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF.</p>
+            </dd>
+             </dl>
+      </div>
+      <div class="col col-6">
+        <dl>
+          <dt>ASF</dt>
+          <dd class="asf">
+            <a href="http://www.apache.org/" target="_self">Foundation</a>
+          </dd>
+          <dd class="asf">
+            <a href="http://www.apache.org/licenses/" target="_self">License</a>
+          </dd>
+          <dd class="asf">
+            <a href="http://www.apache.org/events/current-event" target="_self">Events</a>
+          </dd>
+          <dd class="asf">
+            <a href="http://www.apache.org/foundation/sponsorship.html" target="_self">Sponsorship</a>
+          </dd>
+          <dd class="asf">
+            <a href="http://www.apache.org/foundation/thanks.html" target="_self">Thanks</a>
+          </dd>
+        </dl>
+      </div>
+    </div>
+    <div
+      class="copyright"
+    >Copyright © 2017-2018, The Apache Software Foundation, Apache SkyWalking, SkyWalking, Apache, the Apache feather logo, and the Apache SkyWalking project logo are either registered trademarks or trademarks of the Apache Software Foundation.</div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.footer-container{
+  padding: 2rem 2em 0.3em;
+}
+.footer-container dt{
+  font-size: 16px;
+  font-weight: bold;
+  font-family: Avenir-Medium;
+}
+.footer-container  dd{
+  margin: 0;
+  font-size: 12px;
+  color: #999;
+  line-height: 18px;
+  margin: 8px 0;
+}
+.footer-container .asf{
+  width: 40%;
+  float: left;
+}
+.footer-container  dd a {
+  color: inherit;
+}
+.footer-container .copyright {
+  border-top: 1px solid #eaecef;
+  min-height: 60px;
+  line-height: 20px;
+  text-align: center;
+  font-family: Avenir-Medium;
+  font-size: 12px;
+  color: #999;
+  display: flex;
+  align-items: center;
+}
+.footer-container .cols-container .col-18 {
+    width: 75%;
+    padding-right: 70px;
+    padding-left: 10px;
+}
+.footer-container .cols-container .col-6 {
+    width: 24%;
+    padding-right: 10px;
+    padding-left: 10px;
+}
+.footer-container .cols-container .col {
+    display: inline-block;
+    box-sizing: border-box;
+    vertical-align: top;
+}
+@media screen and (max-width: 640px) {
+  .footer-container .cols-container .col {
+      width: 100%;
+      text-align: center;
+      padding: 0;
+  }
+}
+
+</style>
---
+++ docs/team/README.md
@@ -0,0 +1,58 @@
+# SkyWalking Team
+
+The SkyWalking team is comprised of Members and Contributors. Members have direct access to the source of SkyWalking project and actively evolve the code-base. Contributors improve the project through submission of patches and suggestions to the Members. The number of Contributors to the project is unbounded. All contributions to SkyWalking are greatly appreciated, whether for trivial cleanups, big new features or other material rewards. More details see [here](https://github.com/apache/incubator-skywalking/blob/master/docs/en/guides/README.md).
+
+## Members
+
+Members include PPMC, committer and mentor of SkyWalking project. The List in alphabeta order.
+
+### Incubator Mentor
+
+| Name |Apache ID|
+|:---:|:--:|
+|Ignasi Barrera| nacx |
+|Luke Han| lukehan |
+|Michael Semb Wever| mck |
+|Willem Ning Jiang| ningjiang |
+
+### PPMC
+| Name |Apache ID|
+:---:|:--:|
+|DongXue Si| ilucky |
+|Haoyang Liu| liuhaoyangzz |
+|Hongtao Gao| hanahmily |
+|Sheng Wang| wangsheng |
+|Sheng Wu| wusheng |
+|Kai Wang| wangkai |
+|Yang Bai| baiyang |
+|Yongsheng Peng| pengys |
+|Yuntao Li| liyuntao |
+|Zhang Kewei| zhangkewei |
+|Shinn Zhang| zhangxin |
+
+### Committer
+| Name |Apache ID|
+|:---:|:--:|
+|Can Li| lican |
+|Jian Tan| tanjian |
+|Lang Li| lilang |
+|Yixiong Cao| caoyixiong |
+|Wenbing Wang| wangwenbin |
+
+
+## Contributors
+
+SkyWalking have a lot of Contributors, since we are using GitHub, you could find them in our repository contribution list.
+
+- [SkyWalking main repository](https://github.com/apache/incubator-skywalking/graphs/contributors)
+- [SkyWalking UI](https://github.com/apache/incubator-skywalking-ui/graphs/contributors)
+- [SkyWalking data collect protocol](https://github.com/apache/incubator-skywalking-data-collect-protocol/graphs/contributors)
+- [SkyWalking query protocol](https://github.com/apache/incubator-skywalking-query-protocol/graphs/contributors)
+- [SkyWalking website](https://github.com/apache/incubator-skywalking-website/graphs/contributors)
+
+
+## Becomming a Committer
+
+SkyWalking follows the Apache way to build the community. Anyone can become a committer once they have contributed sufficiently to the project and earned the trust. Read [Contributing Guides](https://github.com/apache/incubator-skywalking/blob/master/docs/en/guides/README.md) to take part in the community.
+
+SkyWalking community follows the Apache Community’s [process](http://community.apache.org/newcommitter.html) on accepting a new committer.
