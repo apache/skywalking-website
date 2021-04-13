ba2021cf64ce77bc01b64bd4f8c574255dd76b69
Juntao Zhang <jtarch@163.com>
2021-04-13 14:16:02 +0800

    Refactor: Add algolia-docsearch (#255)

---
+++ themes/docsy/assets/js/algolia-search.js
@@ -0,0 +1,15 @@
+(function () {
+  var reg = /\/docs\/[a-zA-Z\-]+\/([\w|\.]+)\//;
+  var res = reg.exec(location.href);
+  var version = res && res[1] || "latest";
+  docsearch({
+    apiKey: 'a0589fc2fb9cc4876b9443b2911221bc',
+    indexName: 'skywalking',
+    inputSelector: '#algolia-search-input',
+    algoliaOptions: {
+      'facetFilters': ["version:" + version],
+      hitsPerPage: 8
+    },
+    debug: false
+  });
+})()
