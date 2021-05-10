cd26b2695c3027a4899b1e3ee0a281c9663c8eed
wu-sheng <wu-sheng@users.noreply.github.com>
2021-05-10 02:57:25 +0000

    deploy: 79c30b82ee6ce8dc69e6faadef42af9db7363735

--- js/main.min.ed12e481b1fa7cc9c7f42a7f264902d0468352957892a6dcc895a181e4775900.js
+++ js/main.min.ed12e481b1fa7cc9c7f42a7f264902d0468352957892a6dcc895a181e4775900.js
@@ -9,10 +9,27 @@ event=event||window.event;var target=document.elementFromPoint(event.clientX,eve
 $('.sidebar-button').on('click',function(){$('.sidebar').toggle()
 $('.sidebar-mask').toggle()})
 $('.sidebar-mask').on('click',function(){$('.sidebar-button').trigger('click')})});(function(){var reg=/\/docs\/[a-zA-Z\-]+\/([\w|\.]+)\//;var res=reg.exec(location.href);var version=res&&res[1]||"latest";docsearch({apiKey:'a0589fc2fb9cc4876b9443b2911221bc',indexName:'skywalking',inputSelector:'#algolia-search-input',algoliaOptions:{'facetFilters':["version:"+version],hitsPerPage:8},debug:false});})();$(function(){var $popup=$('.sky-event-popup')
-if(!$popup){return;}
-var key='SkyWalkingPopupClosedTime';var startDate=$popup.data('startdate');var endDate=$popup.data('enddate');if(!isShowed(key)&&isDuringDate(startDate,endDate)){$popup.show()}
-$popup.find('.fa-window-close').on('click',function(){localStorage.setItem(key,new Date());$popup.hide()})
-function isShowed(key){var storageTime=formatDate(localStorage.getItem(key));var now=formatDate(new Date());return storageTime===now;}
-function formatDate(strTime){const date=new Date(strTime);return(date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate());}
-function isDuringDate(startDateStr,endDateStr){var currDate=new Date();var startDate=new Date(startDateStr);var endDate=new Date(endDateStr);if(currDate<=endDate&&(currDate>=startDate||!startDateStr)){return true;}
-return false;}})
+var key='SkyWalkingPopupClosedTime';if(!$popup||isShowed(key)){return;}
+var $items=$popup.find('.item');var $pic=$popup.find('.pic');init()
+function init(){var count=getActiveCount()
+if(!count){return;}
+showPopover(count)
+bindClick()}
+function getActiveCount(){var count=0;$.each($items,function(index,item){var startTime=$(item).data('starttime');var endTime=$(item).data('endtime');if(isDuringTime(startTime,endTime)){$(item).show()
+count++}})
+return count}
+function showPopover(count){var src;if(count===1){$('.sky-event-popup .content-box').addClass('one')
+src=$items.eq(0).data('img')}else{$items.sort(function(a,b){var aDate=new Date($(a).data('endtime'))
+var bDate=new Date($(b).data('endtime'))
+return aDate.getTime()-bDate.getTime()})
+$.each($items,function(index,item){var img=$(item).data('img')
+if(img){src=img;return false}})
+$('.content-box').html($items)}
+if(!src){src=$pic.data('img')
+$pic.css('height','auto')}
+$pic.attr('src',src)
+setTimeout(function(){$popup.show()},1000)}
+function bindClick(){$popup.find('.fa-window-close').on('click',function(){localStorage.setItem(key,new Date());$popup.hide()})}
+function isShowed(key){var storageTime=formatTime(localStorage.getItem(key));var now=formatTime(new Date());return storageTime===now;}
+function formatTime(time){var date=new Date(time);return(date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate());}
+function isDuringTime(startTimeStr,endTimeStr){var currTime=new Date();var startTime=new Date(startTimeStr);var endTime=new Date(endTimeStr);return currTime<=endTime&&(currTime>=startTime||!startTimeStr)}})
