b4f409e73ae8af2d74bd557a6132be84f8a2da9e
wu-sheng <wu-sheng@users.noreply.github.com>
2021-05-29 00:09:02 +0000

    deploy: 8563e481274fda9e6d486f04a77c07fe49074479

--- js/main.min.b39e9e0a8b4cdf9f436c5f7899461300ecea2f05f620805ef970b7832857a4cb.js
+++ js/main.min.b39e9e0a8b4cdf9f436c5f7899461300ecea2f05f620805ef970b7832857a4cb.js
@@ -18,16 +18,23 @@ bindClick()}
 function getActiveCount(){var count=0;$.each($items,function(index,item){var startTime=$(item).data('starttime');var endTime=$(item).data('endtime');if(isDuringTime(startTime,endTime)){$(item).show()
 count++}})
 return count}
-function showPopover(count){var src;if(count===1){$('.sky-event-popup .content-box').addClass('one')
-src=$items.eq(0).data('img')}else{$items.sort(function(a,b){var aDate=new Date($(a).data('endtime'))
+function showPopover(count){var src;var posterSrc;var link;var $contentBox=$popup.find('.content-box');if(count===1){$contentBox.addClass('one')
+src=$items.eq(0).data('img')
+posterSrc=$items.eq(0).data('poster')
+link=$items.eq(0).data('link')}else{$items.sort(function(a,b){var aDate=new Date($(a).data('endtime'))
 var bDate=new Date($(b).data('endtime'))
 return aDate.getTime()-bDate.getTime()})
 $.each($items,function(index,item){var img=$(item).data('img')
+var poster=$(item).data('poster')
+if(poster){posterSrc=poster;link=$(item).data('link');return false}
 if(img){src=img;return false}})
-$('.content-box').html($items)}
-if(!src){src=$pic.data('img')
+$contentBox.html($items)}
+if(posterSrc&&document.body.clientWidth>=440){$pic.attr('src',posterSrc);$pic.on('click',function(){window.location.href=link;$popup.find('.fa-window-close').trigger('click')})
+$('.pic-wrapper').addClass('poster')
+$('.fa-window-close').css('color','#8797ac')
+$contentBox.hide()}else{if(!src){src=$pic.data('img')
 $pic.css('height','auto')}
-$pic.attr('src',src)
+$pic.attr('src',src)}
 setTimeout(function(){$popup.show()},1000)}
 function bindClick(){$popup.find('.fa-window-close').on('click',function(){localStorage.setItem(key,new Date());$popup.hide()})}
 function isShowed(key){var storageTime=formatTime(localStorage.getItem(key));var now=formatTime(new Date());return storageTime===now;}
