98648fc676c591ee00fb4ec220ceea2edd129102
Juntao Zhang <jtarch@163.com>
2021-08-22 19:33:05 +0800

    feat: anchor

---
+++ themes/docsy/assets/js/active.js
@@ -0,0 +1,27 @@
+$(function (){
+  init();
+  function init(){
+    bindClick();
+    setActive();
+  }
+  function bindClick(){
+    $('.title-box a').on('click', function (){
+      var hash = $(this).attr('href')
+      scrollTop(hash);
+    })
+  }
+  function scrollTop(hash){
+    if(!$(hash).length){
+      return;
+    }
+    $('html,body').animate({scrollTop: $(hash).offset().top - 100})
+  }
+  function setActive() {
+    var hash = location.hash;
+    if(!location.hash || !$(hash).length){
+      return;
+    }
+    $(hash).parents('.card').addClass('active');
+    scrollTop(hash)
+  }
+})
