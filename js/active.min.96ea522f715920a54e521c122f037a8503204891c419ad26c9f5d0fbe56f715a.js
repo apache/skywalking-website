f8642fdee77d0e6fff8a9722371d71d514f89c99
wu-sheng <wu-sheng@users.noreply.github.com>
2021-08-29 01:51:54 +0000

    deploy: 2ff256005a9bae44536f84e5774fb178dad0ab28

--- js/active.min.96ea522f715920a54e521c122f037a8503204891c419ad26c9f5d0fbe56f715a.js
+++ js/active.min.96ea522f715920a54e521c122f037a8503204891c419ad26c9f5d0fbe56f715a.js
@@ -1,7 +1,9 @@
 $(function(){init();function init(){bindClick();setActive();}
 function bindClick(){$('.title-box a').on('click',function(){var hash=$(this).attr('href')
-scrollTop(hash);})}
-function scrollTop(hash){if(!$(hash).length){return;}
-$('html,body').animate({scrollTop:$(hash).offset().top-100})}
+scrollTop(hash);})
+$('.more-btn').on('click',function(){var hash=$(this).data('link')
+scrollTop(hash,270);})}
+function scrollTop(hash,offset){if(!$(hash).length){return;}
+$('html,body').animate({scrollTop:$(hash).offset().top-(offset||100)})}
 function setActive(){var hash=location.hash;if(!location.hash||!$(hash).length){return;}
 $(hash).parents('.card').addClass('active');scrollTop(hash)}})
