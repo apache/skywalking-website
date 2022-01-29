38da50dba9f401c8d681c790e57e460ceb9d8615
zhangjuntao <719284178@qq.com>
2022-01-29 21:59:55 +0800

    project list

--- themes/docsy/assets/js/active.js
+++ themes/docsy/assets/js/active.js
@@ -8,6 +8,8 @@ $(function (){
     $('.title-box a').on('click', function (){
       var hash = $(this).attr('href')
       scrollTop(hash);
+      $('.card-wrapper .card').removeClass('active');
+      $(hash).parents('.card').addClass('active');
     })
     $('.more-btn').on('click', function (){
       var hash = $(this).data('link')
@@ -18,7 +20,7 @@ $(function (){
     if(!$(hash).length){
       return;
     }
-    $('html,body').animate({scrollTop: $(hash).offset().top - (offset || 100)})
+    $('html,body').animate({scrollTop: $(hash).offset().top - (offset || 150)})
   }
   function setActive() {
     var hash = location.hash;
