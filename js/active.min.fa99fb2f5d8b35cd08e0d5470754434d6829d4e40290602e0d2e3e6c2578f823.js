155667dab7e7265d3cedce6ab1f7ff1bf4d94299
wu-sheng <wu-sheng@users.noreply.github.com>
2022-02-13 05:53:59 +0000

    deploy: 9686ec9452e12ed94fec0cf1a78f1fe39a218e90

--- js/active.min.fa99fb2f5d8b35cd08e0d5470754434d6829d4e40290602e0d2e3e6c2578f823.js
+++ js/active.min.fa99fb2f5d8b35cd08e0d5470754434d6829d4e40290602e0d2e3e6c2578f823.js
@@ -1,7 +1,6 @@
 $(function(){init();function init(){bindClick();setActive();}
 function bindClick(){$('.title-box a').on('click',function(){var hash=$(this).attr('href')
-scrollTop(hash);$('.card-wrapper .card').removeClass('active');$(hash).parents('.card').addClass('active');});$('.more-btn').on('click',function(){var hash=$(this).data('link')
-scrollTop(hash,270);});$('.link-type').on('click',function(){var hash=$(this).attr('href')
+scrollTop(hash);$('.card-wrapper .card').removeClass('active');$(hash).parents('.card').addClass('active');});$('.link-type').on('click',function(){var hash=$(this).attr('href')
 scrollTop(hash,120);})}
 function scrollTop(hash,offset){if(!$(hash).length){return;}
 $('html,body').animate({scrollTop:$(hash).offset().top-(offset||160)})}
