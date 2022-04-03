839f68361213210f2297540f7719f7d971e99a4b
zhangjuntao <719284178@qq.com>
2022-04-03 08:07:42 +0800

    Upgrade docsearch

--- themes/docsy/assets/js/algolia-search.js
+++ themes/docsy/assets/js/algolia-search.js
@@ -2,14 +2,24 @@ $(function () {
   var reg = /\/docs\/[a-zA-Z\-]+\/([\w|\.]+)\//;
   var res = reg.exec(location.href);
   var version = res && res[1] || "latest";
+  // docsearch({
+  //   apiKey: 'a0589fc2fb9cc4876b9443b2911221bc',
+  //   indexName: 'skywalking',
+  //   inputSelector: '#algolia-search-input',
+  //   algoliaOptions: {
+  //     'facetFilters': ["version:" + version],
+  //     hitsPerPage: 8
+  //   },
+  //   debug: false
+  // });
+
   docsearch({
-    apiKey: 'a0589fc2fb9cc4876b9443b2911221bc',
+    container: '#docsearch',
+    appId: 'X3PCA2KVGM',
     indexName: 'skywalking',
-    inputSelector: '#algolia-search-input',
-    algoliaOptions: {
-      'facetFilters': ["version:" + version],
-      hitsPerPage: 8
+    apiKey: '061374998460e79f66774b9b6905a0dd',
+    searchParameters: {
+      // facetFilters: ["version:" + 'latest'],
     },
-    debug: false
   });
 })
