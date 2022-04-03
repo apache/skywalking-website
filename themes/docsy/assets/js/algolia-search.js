1f74d7291f645e140b4ae9261eeb19730a6ba97b
zhangjuntao <719284178@qq.com>
2022-04-03 08:30:47 +0800

    update

--- themes/docsy/assets/js/algolia-search.js
+++ themes/docsy/assets/js/algolia-search.js
@@ -2,16 +2,6 @@ $(function () {
   var reg = /\/docs\/[a-zA-Z\-]+\/([\w|\.]+)\//;
   var res = reg.exec(location.href);
   var version = res && res[1] || "latest";
-  // docsearch({
-  //   apiKey: 'a0589fc2fb9cc4876b9443b2911221bc',
-  //   indexName: 'skywalking',
-  //   inputSelector: '#algolia-search-input',
-  //   algoliaOptions: {
-  //     'facetFilters': ["version:" + version],
-  //     hitsPerPage: 8
-  //   },
-  //   debug: false
-  // });
 
   docsearch({
     container: '#docsearch',
@@ -20,6 +10,7 @@ $(function () {
     apiKey: '061374998460e79f66774b9b6905a0dd',
     searchParameters: {
       // facetFilters: ["version:" + 'latest'],
+      // hitsPerPage: 8
     },
   });
 })
