f05d56545d12df770c265ac664d740ff4ef03aff
zhangjuntao <719284178@qq.com>
2022-04-04 12:46:35 +0800

    updategs

--- themes/docsy/assets/js/algolia-search.js
+++ themes/docsy/assets/js/algolia-search.js
@@ -8,9 +8,12 @@ $(function () {
     appId: 'X3PCA2KVGM',
     indexName: 'skywalking',
     apiKey: '061374998460e79f66774b9b6905a0dd',
+    // apiKey: 'd8a1be78df7ed6c81d5a8de3714cad31',
+    "hitsPerPage": 10,
     searchParameters: {
-      // facetFilters: ["version:" + 'latest'],
-      // hitsPerPage: 8
+      facetFilters: [["version:" + version]],
+
+      hitsPerPage: 8
     },
   });
 })
