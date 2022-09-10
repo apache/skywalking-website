c58a0929d2848daa9afb6811128db72def1d47cc
Juntao Zhang <719284178@qq.com>
2022-09-10 22:37:04 +0800

    Modify the version alias from `latest` to `Next` (#501)

--- themes/docsy/assets/js/algolia-search.js
+++ themes/docsy/assets/js/algolia-search.js
@@ -1,7 +1,7 @@
 $(function () {
   var reg = /\/docs\/[a-zA-Z\-]+\/([\w|\.]+)\//;
   var res = reg.exec(location.href);
-  var version = res && res[1] || "latest";
+  var version = res && res[1] || "next";
 
   docsearch({
     container: '#docsearch',
