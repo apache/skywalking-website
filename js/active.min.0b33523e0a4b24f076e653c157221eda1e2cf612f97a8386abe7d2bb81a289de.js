9f73c0393b082d3769df98743061848fa93383ae
wu-sheng <wu-sheng@users.noreply.github.com>
2026-03-24 08:24:31 +0000

    deploy: aa7d9a21b8ce8984c4020663321172d97cc7ecfa

--- js/active.min.0b33523e0a4b24f076e653c157221eda1e2cf612f97a8386abe7d2bb81a289de.js
+++
@@ -1,8 +0,0 @@
-$(function(){init();function init(){bindClick();setActive();}
-function bindClick(){$('.container a').not('.link-type').on('click',function(){var hash=$(this).attr('href')
-if(hash&&/^#/.test(hash)){scrollTop(hash);$('.card-wrapper .card').removeClass('active');$(hash).parents('.card').addClass('active');}});$('.link-type').on('click',function(){var hash=$(this).attr('href')
-scrollTop(hash,120);})}
-function scrollTop(hash,offset){if(!$(hash).length){return;}
-$('html,body').animate({scrollTop:$(hash).offset().top-(offset||160)})}
-function setActive(){var hash=location.hash;if(!location.hash||!$(hash).length){return;}
-$(hash).parents('.card').addClass('active');scrollTop(hash)}})
