5b9d1b2949d6ce36e80f61ff2f49d9c3c95d3c56
Hongtao Gao <hanahmily@gmail.com>
2026-08-28 02:40:36 +0000

    Release Apache SkyWalking BanyanDB 0.11.0

--- content/quickstart-docker.sh
+++ content/quickstart-docker.sh
@@ -21,7 +21,7 @@ set -e
 SW_STORAGE=
 
 SW_VERSION=${SW_VERSION:-10.4.0}
-SW_BANYANDB_VERSION=${SW_BANYANDB_VERSION:-0.10.3}
+SW_BANYANDB_VERSION=${SW_BANYANDB_VERSION:-0.11.0}
 
 usage() {
   echo "Usage: quickstart-docker.sh [-f]"
